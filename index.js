f968088ebb71360238f885bc2c8c4b2a710036b0
Derya Kaygisiz <derya.kaygisiz@outlook.com>
2021-05-21 19:59:24 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function createEmployeeRecord (firstName, familyName, title, payPerHour) {
     const obj = {
-        firstName: firstName,
-        familyName: familyName,
-        title: title,
-        payPerHour: payPerHour,
-        timeInEvents: timeInEvents,
-        timeOutEvents: timeOutEvents,
+        firstName: "firstName",
+        familyName: "familyName",
+        title: "title",
+        payPerHour: "payPerHour",
+        timeInEvents: "timeInEvents",
+        timeOutEvents: "timeOutEvents",
     }
     return obj;
 }
