eac80011d1fd7bff9862a58aad4074fce55a71f4
Derya Kaygisiz <derya.kaygisiz@outlook.com>
2021-05-21 20:50:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,12 +16,21 @@ return arrays.map(element => element = createEmployeeRecord(element))
 }
 
 
-function createTimeInEvent (employeeObject, date =["YYYY-MM-DD HHMM"]){
-  const obj = {
+function createTimeInEvent (employeeObject, date = "YYYY-MM-DD HHMM"){
+  let obj = {
     type : "TimeIn",
     hour : parseInt(date.slice(10)),
     date : date.slice(0,11),
-  }
-  employeeObject.createTimeInEvents.push(obj);
+  };
+  employeeObject.timeInEvents.push(obj);
   return employeeObject;
 }
+
+const createTimeInEvent = (employee, dateStamp) => {
+                 employee.timeInEvents.push({
+                                 type: 'TimeIn',
+                                 hour: parseInt(dateStamp.split(' ')[1]),
+                                 date: dateStamp.split(' ')[0],
+                 });
+                 return employee;
+ };
