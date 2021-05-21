cb7e5d44c20d7c7f3767543acfd911080525e7a0
Derya Kaygisiz <derya.kaygisiz@outlook.com>
2021-05-21 20:25:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ function createEmployeeRecord ([firstName, familyName, title, payPerHour]) {
 //     return obj;
 // }
 function createEmployeeRecords (arrays) {
-  arrays.map(element => element = createEmployeeRecord(element))
+return  arrays.map(element => element = createEmployeeRecord(element))
 }
 
 
