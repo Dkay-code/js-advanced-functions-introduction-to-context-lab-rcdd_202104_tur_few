2175d62e8d92233096f15e3525c30f4e214c1595
Derya Kaygisiz <derya.kaygisiz@outlook.com>
2021-05-21 20:40:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,21 +11,29 @@ function createEmployeeRecord ([firstName, familyName, title, payPerHour]) {
 }
 
 
-// function createEmployeeRecords([[employee],[employee]]) {
-//     const obj = {
-//         employee1: [0]
-//     }
-//     return obj;
-// }
 function createEmployeeRecords (arrays) {
 return arrays.map(element => element = createEmployeeRecord(element))
 }
 
 
-// function createEmployeeRecords (Arrays) {
-//  const outputArray = [];
-//      Arrays.forEach(element => {
-//          outputArray.push(createEmployeeRecord(element))
-//      });
-//  return outputArray
-//  }
+function createTimeInEvent (employeeObject, date ="YYYY-MM-DD HHMM"){
+  const obj = {
+    type : "TimeIn",
+    hour : parseInt(date.slice(10)),
+    date : date.slice(0,11),
+  }
+  employeeObject.createTimeInEvents.push(obj);
+
+}
+
+function createEmployeeRecord ([firstName, familyName, title, payPerHour]) {
+    const obj = {
+        firstName: `${firstName}`,
+        familyName: `${familyName}`,
+        title: `${title}`,
+        payPerHour: payPerHour,
+        timeInEvents: [],
+        timeOutEvents: [],
+    }
+    return obj;
+}
